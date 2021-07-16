0e3e716d0567a4c5bc30b93f3065ebfe71605e95
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-16 00:55:49 -0300

    Finalizando movendo os elementos

--- src/main.js
+++ src/main.js
@@ -1,43 +1,61 @@
 (function controller(doc, win) {
   /** Help */
-  // eslint-disable-next-line no-console
+  // eslint-disable-next-line no-console,no-unused-vars
   const log = (message) => win.console.log(message);
 
   /** App */
-  // For draggable
-  const dragstart = () => {
-    log('> CARD: Star dragging');
-  };
-
-  const drag = () => {
-    log('> CARD: Is dragging');
-  };
-
-  const dragend = () => {
-    log('> CARD: Stop drag!');
-  };
-
-  // For dropzone
-  const dragenter = () => {
-    log('> DROPZONE: Enter in zone!');
-  };
-
-  const dragover = () => {
-    log('> DROPZONE: In zone!');
-  };
-
-  const dragleave = () => {
-    log('> DROPZONE: Leave the zone!');
-  };
-
-  const drop = () => {
-    log('> DROPZONE: Dropped!');
-  };
-
-  /** Control DOM */
+  // Selecting nodes
   const cards = doc.querySelectorAll('.card');
   const dropZones = doc.querySelectorAll('.dropzone');
 
+  // For .card
+  function dragstart() {
+    // log('> CARD: Star dragging');
+    dropZones.forEach((dropzone) => dropzone.classList.add('highlight'));
+
+    this.classList.add('is-dragging');
+  }
+
+  function drag() {
+    // log('> CARD: Is dragging');
+  }
+
+  function dragend() {
+    // log('> CARD: Stop drag!');
+    dropZones.forEach((dropzone) => dropzone.classList.remove('highlight'));
+
+    this.classList.remove('is-dragging');
+  }
+
+  // For .dropzone
+  function dragenter() {
+    // log('> DROPZONE: Enter in zone!');
+  }
+
+  function dragover() {
+    // log('> DROPZONE: In zone!');
+
+    this.classList.add('over');
+
+    // Get dragging card
+    const cardBeingDragged = doc.querySelector('.is-dragging');
+
+    // Append card
+    this.appendChild(cardBeingDragged);
+  }
+
+  function dragleave() {
+    // log('> DROPZONE: Leave the zone!');
+
+    this.classList.remove('over');
+  }
+
+  function drop() {
+    // log('> DROPZONE: Dropped!');
+    this.classList.remove('over');
+  }
+
+  /** Adding events */
   cards.forEach((card) => {
     card.addEventListener('dragstart', dragstart, false);
     card.addEventListener('drag', drag, false);
